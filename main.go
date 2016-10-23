53f26d9f85adb2eada8dfac4f64abdb7f147c5db
adesokanayo <adesokano@accessbankplc.com>
2016-10-23 20:52:42 +0100

    new home page

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/adesokanayo/mentorsng/views"
 	"github.com/gorilla/mux"
 )
+
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+
+var homeView views.View
+var contactView *views.View
 
 func main() {
 	port := os.Getenv("PORT")
@@ -14,19 +22,41 @@ func main() {
 		port = "8080"
 	}
 
+	//homeView := views.NewView("bootstrap", "views/home.gohtml")
+	//contactView := views.NewView("bootstrap", "views/contact.gohtml")
+
+	var err error
+	homeTemplate, err = template.ParseFiles(
+		"views/home.gohtml",
+		"views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles(
+		"views/contact.gohtml",
+		"views/layouts/footer.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	Routes := mux.NewRouter()
 	Routes.HandleFunc("/home", home)
 
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
 
-	http.Handle("/", Routes)
+	http.Handle("/s", Routes)
 	http.ListenAndServe(":"+port, nil)
 	log.Println("Listening...")
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-
 	w.Header().Set("Content-Type", "text/html")
-	log.Println("called something")
+	homeTemplate.ExecuteTemplate(w, homeView.Layout, nil)
 
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	contactTemplate.ExecuteTemplate(w, contactView.Layout, nil)
 }
--- views/view.go
+++ views/view.go
@@ -2,10 +2,11 @@ package views
 
 import "html/template"
 
-func NewView(files ...string) *View {
+func NewView(layout string, files ...string) *View {
 
 	//var files string
 	files = append(files, "views/layout/footer.gohtml")
+	files = append(files, "views/layout/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -18,4 +19,5 @@ func NewView(files ...string) *View {
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
